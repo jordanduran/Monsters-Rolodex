a070af39788020cd3928322b00bdedeb120a1bbd
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2019-12-28 20:28:11 -0500

    Updates

---
+++ precache-manifest.6f776bfb4a4fb342364f8a850817f53f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e938a0f09bf39ff9eab9cdff6525df32",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "d0b38940cc7ac4275241",
+    "url": "/Monsters-Rolodex/static/css/main.ff8415e0.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/Monsters-Rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters-Rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0b38940cc7ac4275241",
+    "url": "/Monsters-Rolodex/static/js/main.064e349e.chunk.js"
+  },
+  {
+    "revision": "158fd0877078ccfb2131",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.f6db19d2.js"
+  }
+]);
